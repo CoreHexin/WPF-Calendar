aa2752ff8c8f553250f2624712c9321a1d72be50
Hexin <hexin.email@qq.com>
2025-06-10 23:03:33 +0800

    添加项目文件。

---
+++ WPF-Calendar/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF_Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void addNoteLabel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            addNoteTextbox.Focus();
+        }
+
+        private void addNoteTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(addNoteTextbox.Text))
+            {
+                addNoteLabel.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                addNoteLabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void timeLabel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            timeTextBox.Focus();
+        }
+
+        private void timeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(timeTextBox.Text))
+            {
+                timeLabel.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                timeLabel.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ WPF-Calendar/UserControls/MenuButton.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using FontAwesome.WPF;
+
+namespace WPF_Calendar.UserControls
+{
+    public partial class MenuButton : UserControl
+    {
+        public MenuButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register("Caption", typeof(string), typeof(MenuButton));
+
+
+        public FontAwesomeIcon Icon
+        {
+            get { return (FontAwesomeIcon)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(FontAwesomeIcon), typeof(MenuButton));
+
+    }
+}
